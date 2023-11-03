0933be4abbafd04887f79fc075f54a0da354dcbe
Gustavs <gustavssteinbergs@grobinasskola.com>
2023-11-03 10:49:24 +0200

    ooo

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,21 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
+1) RSS?
+2) plūsmu no google.lv
 
 """
+import feedparser
+
+# URL uz RSS plūsmu
+
+rss_url='https://news.google.com/home?hl=lv&gl=LV&ceid=LV:lv'
+
+#iegūstam RSS plūsmas datus un veicas analīzi...
+kkk=feedparser.parse(rss_url)
+
+# noformēšana....
+for entry in kkk.entries:
+    print("Virsraksts", entry.title)
+    print("Saite:", entry.link)
+    print("Publicēšanas datums: ", entry.published)
+    print("\n")
